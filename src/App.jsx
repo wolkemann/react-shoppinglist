bc072512a198f7114fe0465317128f6df176595a
Federico Sardo <federico.sardo1@gmail.com>
2022-03-04 17:03:39 +0100

    added localstorage support

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 /*
-Importing base CSS
+Importing base CSS and Images
 */
 import './css/App.css';
+import './css/shoppingList.css';
+import './css/shoppingItem.css';
 import './css/variables.css';
+import mama from './components/homeIcon-trimmy.png';
 
 /*
 Importing Components
 */
 import { Header } from './components/Header/Header.jsx';
-import { ShoppingList } from './components/ShoppingList.jsx';
 import { Finder } from './components/Finder/Finder.jsx';
 
 /*
@@ -18,7 +20,16 @@ import { useState, useEffect } from 'react';
 import { items as listDB } from './db.jsx';
 
 function App() {
-  const [shoppingChart, setShoppingChart] = useState([]);
+  const [shoppingChart, setShoppingChart] = useState(() => {
+    const saved = localStorage.getItem('shoppingChart');
+    const initialValue = JSON.parse(saved);
+    return initialValue || [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('shoppingChart', JSON.stringify(shoppingChart));
+  }, [shoppingChart]);
+
   function handleFinderPressEnter(e) {
     e.preventDefault();
     setShoppingChart([
@@ -32,7 +43,37 @@ function App() {
     <>
       <Header />
       <main>
-        <ShoppingList productList={shoppingChart} />
+        <section>
+          <div className="ShoppingList__list">
+            {shoppingChart.length === 0 ? (
+              <div>
+                <img src={mama} className="ShoppingList__mama" />
+                <p className="ShoppingList__emptyText">
+                  Nothing to buy, Mama si angry.
+                </p>
+              </div>
+            ) : (
+              shoppingChart.map(element => {
+                return (
+                  <div
+                    key={element.id}
+                    className="ShoppingItem"
+                    onClick={() => {
+                      setShoppingChart(() => {
+                        return shoppingChart.filter(a => {
+                          console.log(a.id, element.id);
+                          return a.id !== element.id;
+                        });
+                      });
+                    }}
+                  >
+                    {element.name}
+                  </div>
+                );
+              })
+            )}
+          </div>
+        </section>
         <Finder formEventHandler={handleFinderPressEnter} />
       </main>
     </>
--- src/components/ShoppingItem/ShoppingItem.jsx
+++
@@ -1,5 +0,0 @@
-import './ShoppingItem.css';
-
-export function ShoppingItem({ name }) {
-  return <div className="ShoppingItem">{name}</div>;
-}
