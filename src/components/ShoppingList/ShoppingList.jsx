9f41985250865748eadb7440b969582c49e571cd
wolkemann <ohtoristudent@gmail.com>
2022-03-05 16:57:35 +0100

    fixed ShoppingList component

--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -3,36 +3,34 @@ import mama from './homeIcon-trimmy.png';
 
 export function ShoppingList({ currentShoppingItems, handleSingleItem }) {
   return (
-    <section>
-      <div className="ShoppingList__list">
-        {currentShoppingItems.length === 0 ? (
-          <div>
-            <img src={mama} className="ShoppingList__mama" />
-            <p className="ShoppingList__emptyText">
-              Nothing to buy, Mama si angry.
-            </p>
-          </div>
-        ) : (
-          currentShoppingItems.map(element => {
-            return (
-              <div
-                key={element.id}
-                className="ShoppingItem"
-                onClick={() => {
-                  handleSingleItem(() => {
-                    return currentShoppingItems.filter(a => {
-                      console.log(a.id, element.id);
-                      return a.id !== element.id;
-                    });
+    <section className="ShoppingList__list">
+      {currentShoppingItems.length === 0 ? (
+        <div>
+          <img src={mama} className="ShoppingList__mama" />
+          <p className="ShoppingList__emptyText">
+            Nothing to buy, Mama si angry.
+          </p>
+        </div>
+      ) : (
+        currentShoppingItems.map(element => {
+          return (
+            <div
+              key={element.id}
+              className="ShoppingItem"
+              onClick={() => {
+                handleSingleItem(() => {
+                  return currentShoppingItems.filter(a => {
+                    console.log(a.id, element.id);
+                    return a.id !== element.id;
                   });
-                }}
-              >
-                {element.name}
-              </div>
-            );
-          })
-        )}
-      </div>
+                });
+              }}
+            >
+              {element.name}
+            </div>
+          );
+        })
+      )}
     </section>
   );
 }
